26c485af345d9b84eef1155263be3dd2eae11216
Bruno <bm.julio1994@gmail.com>
2024-11-08 11:49:31 -0300

    Adicionado file frank

---
+++ Frank.py
@@ -0,0 +1,35 @@
+#calculadora
+#agenda telefonica
+#silaba tonica
+
+#Variables
+##entradas
+num1 = 10
+num2 = 2
+#processamento
+#soma
+soma = num1+num2
+#subtr
+subtr = num1-num2
+#multi
+multi = num1*num2
+#exponencial
+expo = num1**num2
+#divisao inteira
+div_int = num1//num2
+#modulo
+resto = num1%num2
+#div
+div = num1/num2
+
+#saidas
+print("Calculadora")
+
+print(soma)
+print(subtr)
+print(multi)
+print(expo)
+print(div)
+print(div_int)
+print(resto)
+
